b016d34328fa920afff2ede885e3be65ad17386c
Mike Roy <mikeroy06@bellsouth.net>
2017-09-27 08:45:11 -0400

    twitter-page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,9 +5,9 @@ class Tweet < ApplicationRecord
     has_many :tags, through: :tweet_tags
 
     before_validation :link_check, on: :create
-    validates :message, length: {maximum: 140, too_long: "Max is 140!"}
+    validates :message, length: {maximum: 140, too_long: "Max is 140!"}, on: :create
     after_validation :apply_link, on: :create
-private
+
        def link_check
       check = false
       if self.message.include? "http://" 
@@ -31,7 +31,7 @@ private
       end
 			def apply_link
 				arr = self.message.split
-				index = arr.map { |x| x.include?("http://") || x.include?("https://") }.index(true)
+				index = arr.map { |x| x.include?("http://") || x.include?("https://") || x.include?("www.")}.index(true)
 
 				if index
 					url = arr[index]
